20f72b2bcf02f9a9cfe3112baf947cef787b317d
Ángel Marín <35677091+SilverKnightAnnt@users.noreply.github.com>
2019-03-16 21:59:16 -0600

    Finnished participation to focus group

--- src/main/webapp/app/entities/focus-group/participate.component.ts
+++ src/main/webapp/app/entities/focus-group/participate.component.ts
@@ -3,15 +3,17 @@ import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
 import {Subscription} from 'rxjs';
 import {filter, map} from 'rxjs/operators';
 import {JhiEventManager, JhiAlertService} from 'ng-jhipster';
-
 import {IFocusGroup} from 'app/shared/model/focus-group.model';
-import {AccountService, IUser, UserService} from 'app/core';
+import {AccountService, UserService} from 'app/core';
 import {FocusGroupService} from './focus-group.service';
 import {UserAppService} from 'app/entities/user-app';
 import {ParticipantService} from 'app/entities/participant';
-import {IParticipant, Participant} from 'app/shared/model/participant.model';
+import {IParticipant} from 'app/shared/model/participant.model';
 import {ITestResult, TestResult} from 'app/shared/model/test-result.model';
 import {TestResultService} from 'app/entities/test-result';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {TestQuestionService} from 'app/entities/test-question';
+import {ITestQuestion} from 'app/shared/model/test-question.model';
 
 @Component({
     selector: 'jhi-participate',
@@ -20,12 +22,13 @@ import {TestResultService} from 'app/entities/test-result';
 export class ParticipateComponent implements OnInit, OnDestroy {
     focusGroups: IFocusGroup[];
     testResults: ITestResult[];
+    testQuestions: ITestQuestion[];
     focusGroup: IFocusGroup;
     participant: IParticipant;
     testResult = new TestResult('', this.focusGroup, this.participant);
     currentAccount: any;
     eventSubscriber: Subscription;
-    asd2: number;
+    actualUserId: number;
 
     constructor(
         protected focusGroupService: FocusGroupService,
@@ -35,11 +38,13 @@ export class ParticipateComponent implements OnInit, OnDestroy {
         protected userService: UserService,
         protected userAppService: UserAppService,
         protected participantService: ParticipantService,
-        protected testResultsService: TestResultService
+        protected testResultsService: TestResultService,
+        protected testQuestionsService: TestQuestionService,
+        protected modalService: NgbModal
     ) {
     }
 
-    loadAll() {
+    loadAllFocusGroups() {
         this.focusGroupService
             .query()
             .pipe(
@@ -65,14 +70,14 @@ export class ParticipateComponent implements OnInit, OnDestroy {
             .subscribe(
                 (res: ITestResult[]) => {
                     this.testResults = res;
-                    this.testX();
+                    this.findActualParticipant();
                 },
                 (res: HttpErrorResponse) => this.onError(res.message)
             );
     }
 
     ngOnInit() {
-        this.loadAll();
+        this.loadAllFocusGroups();
         this.accountService.identity().then(account => {
             this.currentAccount = account;
         });
@@ -88,48 +93,110 @@ export class ParticipateComponent implements OnInit, OnDestroy {
     }
 
     registerChangeInFocusGroups() {
-        this.eventSubscriber = this.eventManager.subscribe('focusGroupListModification', response => this.loadAll());
+        this.eventSubscriber = this.eventManager.subscribe('focusGroupListModification', response => this.loadAllFocusGroups());
     }
 
     protected onError(errorMessage: string) {
         this.jhiAlertService.error(errorMessage, null, null);
     }
 
-    participate(asd) {
-        this.focusGroupService.find(asd).subscribe(data3 => {
-            this.focusGroup = data3.body;
+    askParticipation(group, modalToDisplay) {
+        this.focusGroupService.find(group).subscribe(groupFound => {
+            this.focusGroup = groupFound.body;
             this.testResult.participant = this.participant;
             this.testResult.focusGroup = this.focusGroup;
-            this.testResult.grade = '100';
-            this.testResultsService.create(this.testResult).subscribe(data => {
-                this.loadAllTests();
-            });
+            if (this.focusGroup.aptitudeTest != null) {
+                this.findQuestionsByAptitudeTests();
+                this.openModal(modalToDisplay);
+            } else {
+                this.testResult.grade = '100';
+                this.testResultsService.create(this.testResult).subscribe(data => {
+                    this.loadAllTests();
+                });
+            }
         });
 
     }
 
-    testX() {
-        this.currentAccount = this.userService.getUserWithAuthorities().forEach(erg => {
-                this.asd2 = erg.id;
-                this.userAppService.findByUserId(erg.id).subscribe(data => {
-                    const bla2 = data.id;
-                    this.participantService.findByUser(bla2).subscribe(data2 => {
-                        this.asd2 = data2.id;
-                        this.participant = data2;
-                        this.amI();
+    findQuestionsByAptitudeTests() {
+        this.testQuestionsService.findAllByAptituteTest(this.focusGroup.aptitudeTest.id)
+            .pipe(
+                filter((res: HttpResponse<ITestQuestion[]>) => res.ok),
+                map((res: HttpResponse<ITestQuestion[]>) => res.body)
+            )
+            .subscribe(
+                (res: ITestQuestion[]) => {
+                    this.testQuestions = res;
+                },
+                (res: HttpErrorResponse) => this.onError(res.message)
+            );
+    }
+
+    findActualParticipant() {
+        this.currentAccount = this.userService.getUserWithAuthorities().forEach(jhiUser => {
+                this.actualUserId = jhiUser.id;
+                this.userAppService.findByUserId(jhiUser.id).subscribe(userApp => {
+                    const participant = userApp.id;
+                    this.participantService.findByUser(participant).subscribe(foundParticipant => {
+                        this.actualUserId = foundParticipant.id;
+                        this.participant = foundParticipant;
+                        this.findActualUserInOnHoldQueue();
                     });
                 });
             }
         );
     }
 
-    amI() {
-        const list = this.testResults;
-        for (let i = 0; i < list.length; i++) {
-            if (this.participant.id === list[i].participant.id) {
-                document.getElementById('btn-' + list[i].focusGroup.id).hidden = true;
+    findActualUserInOnHoldQueue() {
+        const testResultsList = this.testResults;
+        for (let row = 0; row < testResultsList.length; row++) {
+            if (this.participant.id === testResultsList[row].participant.id) {
+                document.getElementById('btn-' + testResultsList[row].focusGroup.id).hidden = true;
             }
         }
+    }
+
+    openModal(content) {
+        this.modalService.open(content).result.then(result => {
+            console.log(`Closed with: ${result}`);
+        }, reason => {
+            console.log(`Dismissed ${this.getDismissReason(reason)}`);
+        });
+    }
+
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return `with: ${reason}`;
+        }
+    }
+
+    sendAptitudeTestAnswers(form) {
+        this.testResult.grade = this.calculateAverageGrade(form).toString();
+        this.testResultsService.create(this.testResult).subscribe(data => {
+            this.loadAllTests();
+        });
+    }
+
+    calculateAverageGrade(form): number {
+        let grade = 0;
+        for (let test = 0; test < form.childNodes[1].children.length; test++) {
+            const elementAnswers = form.childNodes[1].children[test].getElementsByTagName('INPUT');
+            for (let answer = 0; answer < elementAnswers.length; answer++) {
+                if (elementAnswers[answer].checked === true) {
+                    if (this.testQuestions[test].answers[answer].desired === true) {
+                        grade = grade + 100;
+                    } else {
+                        grade = grade + 65;
+                    }
+                }
+            }
+        }
+        grade = grade / this.testQuestions.length;
+        return grade;
     }
 
 }
