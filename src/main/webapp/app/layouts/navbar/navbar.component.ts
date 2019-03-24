424849911514e55c4cf41636829d6f2c3425ea16
Ángel Marín <35677091+SilverKnightAnnt@users.noreply.github.com>
2019-03-23 23:35:04 -0600

    Fixed navbar permissions

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 
-import { VERSION } from 'app/app.constants';
-import { AccountService, LoginModalService, LoginService } from 'app/core';
-import { ProfileService } from 'app/layouts/profiles/profile.service';
+import {VERSION} from 'app/app.constants';
+import {AccountService, LoginModalService, LoginService, UserService} from 'app/core';
+import {ProfileService} from 'app/layouts/profiles/profile.service';
+import {IUserApp, UserApp} from 'app/shared/model/user-app.model';
 
 @Component({
     selector: 'jhi-navbar',
@@ -18,13 +19,16 @@ export class NavbarComponent implements OnInit {
     swaggerEnabled: boolean;
     modalRef: NgbModalRef;
     version: string;
+    user: IUserApp;
+    currentAccount: any;
 
     constructor(
         private loginService: LoginService,
         private accountService: AccountService,
         private loginModalService: LoginModalService,
         private profileService: ProfileService,
-        private router: Router
+        private router: Router,
+        private userService: UserService
     ) {
         this.version = VERSION ? 'v' + VERSION : '';
         this.isNavbarCollapsed = true;
@@ -61,5 +65,42 @@ export class NavbarComponent implements OnInit {
 
     getImageUrl() {
         return this.isAuthenticated() ? this.accountService.getImageUrl() : null;
+    }
+
+    findActualUser() {
+        this.currentAccount = this.userService.getUserWithAuthorities().forEach(jhiUser => {
+            let permissions: string[] = [];
+            for (let i = 0; i < jhiUser.authorities.length; i++) {
+                switch (jhiUser.authorities[i]) {
+                    case 'ROLE_ADMIN':
+                        permissions = ['userAppPermissions', 'participantPermissions', 'institutionPermissions',
+                            'incentivePermissions', 'aptitudeTestsPermissions', 'testResultPermissions', 'testQuestionPermissions',
+                            'testAnswerPermissions', 'focusGroupPermissions', 'meetingPermissions', 'membershipPermissions',
+                            'paymentMethodPermissions', 'systemVariablePermissions', 'categoryPermissions', 'balancePermissions',
+                            'paymentPermissions'];
+                        break;
+                    case 'ROLE_PARTICIPANT':
+                        permissions = ['paymentMethodPermissions', 'balancePermissions', 'paymentPermissions'];
+                        break;
+                    case 'ROLE_INSTITUTION':
+                        permissions = ['incentivePermissions', 'aptitudeTestsPermissions', 'testResultPermissions', 'testQuestionPermissions',
+                            'testAnswerPermissions', 'focusGroupPermissions', 'meetingPermissions', 'membershipPermissions',
+                            'paymentMethodPermissions', 'categoryPermissions', 'balancePermissions', 'paymentPermissions'];
+                        break;
+                    case 'ROLE_SUBADMIN':
+                        permissions = ['userAppPermissions', 'participantPermissions', 'institutionPermissions',
+                            'incentivePermissions', 'aptitudeTestsPermissions', 'testResultPermissions', 'testQuestionPermissions',
+                            'testAnswerPermissions', 'focusGroupPermissions', 'meetingPermissions', 'membershipPermissions',
+                            'paymentMethodPermissions', 'systemVariablePermissions', 'categoryPermissions', 'balancePermissions',
+                            'paymentPermissions'];
+                        break;
+                    default:
+                        break;
+                }
+            }
+            for (let j = 0; j < permissions.length; j++) {
+                document.getElementById(permissions[j]).hidden = false;
+            }
+        });
     }
 }
