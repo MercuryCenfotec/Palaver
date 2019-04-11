3bbb423b62a6b83d204545e80799a01d1ca77333
Carlos Madrigal <cmadrigaln@ucenfotec.ac.cr>
2019-04-10 20:32:07 -0600

    incentive form

---
+++ src/main/webapp/app/entities/incentive/incentive-form.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { IIncentive } from 'app/shared/model/incentive.model';
+import { IncentiveService } from './incentive.service';
+
+@Component({
+    selector: 'jhi-incentive-form',
+    templateUrl: './incentive-form.component.html'
+})
+export class IncentiveUpdateComponent implements OnInit {
+    incentive: IIncentive;
+    isSaving: boolean;
+
+    constructor(protected incentiveService: IncentiveService, protected activatedRoute: ActivatedRoute) {}
+
+    ngOnInit() {
+        this.isSaving = false;
+        this.activatedRoute.data.subscribe(({ incentive }) => {
+            this.incentive = incentive;
+        });
+    }
+
+    previousState() {
+        window.history.back();
+    }
+
+    save() {
+        this.isSaving = true;
+        if (this.incentive.id !== undefined) {
+            this.subscribeToSaveResponse(this.incentiveService.update(this.incentive));
+        } else {
+            this.subscribeToSaveResponse(this.incentiveService.create(this.incentive));
+        }
+    }
+
+    protected subscribeToSaveResponse(result: Observable<HttpResponse<IIncentive>>) {
+        result.subscribe((res: HttpResponse<IIncentive>) => this.onSaveSuccess(), (res: HttpErrorResponse) => this.onSaveError());
+    }
+
+    protected onSaveSuccess() {
+        this.isSaving = false;
+        this.previousState();
+    }
+
+    protected onSaveError() {
+        this.isSaving = false;
+    }
+}
