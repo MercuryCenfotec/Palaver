f3696f264822835ef8e663e09fa37fe01c133b1a
Ángel Marín <35677091+SilverKnightAnnt@users.noreply.github.com>
2019-04-13 20:30:04 -0600

    Fixed notification logic

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {ModalDismissReasons, NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 
-import { VERSION } from 'app/app.constants';
+import {VERSION} from 'app/app.constants';
 import {AccountService, IUser, LoginModalService, LoginService, UserService} from 'app/core';
-import { ProfileService } from 'app/layouts/profiles/profile.service';
-import { IUserApp, UserApp } from 'app/shared/model/user-app.model';
-import { IParticipant, Participant } from 'app/shared/model/participant.model';
-import { ParticipantService } from 'app/entities/participant';
-import { UserAppService } from 'app/entities/user-app';
-import { InstitutionService } from 'app/entities/institution';
-import { IInstitution, Institution } from 'app/shared/model/institution.model';
+import {ProfileService} from 'app/layouts/profiles/profile.service';
+import {IUserApp, UserApp} from 'app/shared/model/user-app.model';
+import {IParticipant, Participant} from 'app/shared/model/participant.model';
+import {ParticipantService} from 'app/entities/participant';
+import {UserAppService} from 'app/entities/user-app';
+import {InstitutionService} from 'app/entities/institution';
+import {IInstitution, Institution} from 'app/shared/model/institution.model';
 import {NotificationService} from "app/entities/notification";
 import {tmpdir} from "os";
 import {filter, map} from "rxjs/operators";
@@ -20,6 +20,7 @@ import {INotification} from "app/shared/model/notification.model";
 import {JhiAlertService} from "ng-jhipster";
 import {FocusGroupService} from "app/entities/focus-group";
 import {IFocusGroup} from "app/shared/model/focus-group.model";
+import {BanService} from "app/entities/ban";
 
 @Component({
     selector: 'jhi-navbar',
@@ -39,6 +40,8 @@ export class NavbarComponent implements OnInit {
     institution = new Institution(null, '', '', '', '', null);
     userNotifications: INotification[] = [];
     obt: IFocusGroup;
+    done: boolean = false;
+    ban: IBan;
 
     constructor(
         private loginService: LoginService,
@@ -52,14 +55,17 @@ export class NavbarComponent implements OnInit {
         private institutionService: InstitutionService,
         private notificationService: NotificationService,
         protected jhiAlertService: JhiAlertService,
-        protected focusGroupService: FocusGroupService
+        protected focusGroupService: FocusGroupService,
+        protected banService: BanService,
+        protected modalService: NgbModal
     ) {
         this.version = VERSION ? 'v' + VERSION : '';
         this.isNavbarCollapsed = true;
     }
 
     ngOnInit() {
-        this.currentAccount = this.userService.getUserWithAuthorities().forEach(jhiUser => {});
+        this.currentAccount = this.userService.getUserWithAuthorities().forEach(jhiUser => {
+        });
 
         this.profileService.getProfileInfo().then(profileInfo => {
             this.inProduction = profileInfo.inProduction;
@@ -218,18 +224,88 @@ export class NavbarComponent implements OnInit {
             );
     }
 
-    obtainGroup(focusGroupId: number): IFocusGroup {
-        debugger
-        let obt: IFocusGroup;
-        this.focusGroupService.find(focusGroupId).subscribe( getIt => {
-            obt = getIt.body;
-        });
-        return obt;
-    }
-
     getNotifications() {
         if (JSON.parse(localStorage.getItem("notifications")) != null) {
             this.userNotifications = JSON.parse(localStorage.getItem("notifications"));
         }
+    }
+
+    doThis() {
+        if (this.done === false) {
+            let ele = document.getElementById('plsWork');
+            let node = document.createElement('DIV');
+            for (let i = 0; i < this.userNotifications.length; i++) {
+                switch (this.userNotifications[i].type) {
+                    case 'GroupAccepted':
+                        this.focusGroupService.find(this.userNotifications[i].messageId).subscribe(getIt => {
+                            node.innerHTML = node.innerHTML + '<a class="dropdown-item noti-container py-3">\n' +
+                                '                                        <i class="ft-thumbs-up success float-left d-block font-large-1 mt-4 mr-4"></i>\n' +
+                                '                                        <span class="noti-wrapper">\n' +
+                                '                                        <span class="noti-title line-height-1 d-block text-bold-400 success">¡Felicidades!</span>\n' +
+                                '                                        <span class="noti-text">Has sido aceptado en el grupo de enfoque:</span><br>\n' +
+                                '                                        <span class="noti-text">' + getIt.body.name + '</span>\n' +
+                                '                                    </span>\n' +
+                                '                                    </a>';
+                            ele.appendChild(node);
+                        });
+                        break;
+                    case 'GroupRejected':
+                        this.focusGroupService.find(this.userNotifications[i].messageId).subscribe(getIt => {
+                            node.innerHTML = node.innerHTML + '<a class="dropdown-item noti-container py-3">\n' +
+                                '                                        <i class="ft-user-x danger float-left d-block font-large-1 mt-4 mr-4"></i>\n' +
+                                '                                        <span class="noti-wrapper">\n' +
+                                '                                        <span class="noti-title line-height-1 d-block text-bold-400 danger">¡Lo sentimos!</span>\n' +
+                                '                                        <span class="noti-text">No te aceptaron en el grupo de enfoque:</span><br>\n' +
+                                '                                        <span class="noti-text">' + getIt.body.name + '</span>\n' +
+                                '                                    </span>\n' +
+                                '                                    </a>';
+                            ele.appendChild(node);
+                        });
+                        break;
+                    case 'GroupExpulsion':
+                        this.banService.find(this.userNotifications[i].messageId).subscribe(getIt => {
+                            node.innerHTML = node.innerHTML + '<a class="dropdown-item noti-container py-3">\n' +
+                                '                                        <i class="ft-alert-octagon warning float-left d-block font-large-1 mt-4 mr-4"></i>\n' +
+                                '                                        <span class="noti-wrapper">\n' +
+                                '                                        <span class="noti-title line-height-1 d-block text-bold-400 warning">¡Lo sentimos!</span>\n' +
+                                '                                        <span class="noti-text">Has sido expulsado de un grupo de enfoque.</span><br>\n' +
+                                '                                        <span class="noti-text">Ve a la sección de expulsiones para ver el detalle</span>\n' +
+                                '                                    </span>\n' +
+                                '                                    </a>';
+                            ele.appendChild(node);
+                        });
+                        break;
+                    case 'CallStart':
+                        node.innerHTML = node.innerHTML + '<a class="dropdown-item noti-container py-3">\n' +
+                            '                                        <i class="ft-video info float-left d-block font-large-1 mt-4 mr-4"></i>\n' +
+                            '                                        <span class="noti-wrapper">\n' +
+                            '                                        <span class="noti-title line-height-1 d-block text-bold-400 info">¡Videollamada iniciada!</span>\n' +
+                            '                                        <span class="noti-text">Ve al calendario para poder unirte.</span>\n' +
+                            '                                    </span>\n' +
+                            '                                    </a>';
+                        ele.appendChild(node);
+                        break;
+
+                    case 'PaymentDone':
+                        node.innerHTML = node.innerHTML + '<a class="dropdown-item noti-container py-3">\n' +
+                            '                                        <i class="ft-shopping-cart success float-left d-block font-large-1 mt-4 mr-4"></i>\n' +
+                            '                                        <span class="noti-wrapper">\n' +
+                            '                                        <span class="noti-title line-height-1 d-block text-bold-400 success">¡Transacción realizada!</span>\n' +
+                            '                                        <span class="noti-text">Tienes nuevos fondos en tu cuenta</span>\n' +
+                            '                                    </span>\n' +
+                            '                                    </a>';
+                        ele.appendChild(node);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            this.done = true;
+        }
+    }
+
+    showMe() {
+        debugger
+        console.log('Hee hee');
     }
 }
